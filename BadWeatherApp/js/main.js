43b61254b81a27da7e62e20626af7a4a74f88295
Jivka Ignatova <ignatova.jivka@gmail.com>
2018-02-22 16:07:14 +0200

    automated dropdown menu

--- BadWeatherApp/js/main.js
+++ BadWeatherApp/js/main.js
@@ -1,13 +1,28 @@
 function onDataRetrieved(json){
+    console.log(json);
     var cityName = $("#city-name");
-    cityName.html("London");
+    cityName.html(json.city.name);
     var countryName = $("#country-name");
-    countryName.html("England");
+    countryName.html(json.city.country);
+
 }
 
-function onButtonClick() {
-    $.get("http://cors-anywhere.herokuapp.com/samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22", onDataRetrieved);
+function onButtonClick(cityId) {
+//  $.get(`http://api.openweathermap.org/data/2.5/forecast?q=London,us&appid=2efb9211ec2c1db3d00ea14c0d24c30d`, onDataRetrieved);
+//  $.get(`http://samples.openweathermap.org/data/2.5/forecast?id=${cityId}&appid=b6907d289e10d714a6e88b30761fae22`, onDataRetrieved);
+    $.get(`http://api.openweathermap.org/data/2.5/forecast?id=${cityId}&appid=2efb9211ec2c1db3d00ea14c0d24c30d`, onDataRetrieved);
 }
 
-$("#london-city").on("click", onButtonClick);
+// $(".dropdown-menu").children().on("click", function(){
+//     let $this = $(this);
+//     // let cityId = $this.html();
+    
+//     onButtonClick(cityId);
+// });
+
+$(".dropdown-menu").on("click", "a", function(){
+    let cityId = $(this).attr("id");
+    console.log(cityId);
+    onButtonClick(cityId);
+});
 
