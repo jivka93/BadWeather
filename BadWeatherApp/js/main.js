c54816f71a3b8492f7d14dd316609887805e12bc
Nikolay Nikolov <nikolaytsnikolov@gmail.com>
2018-02-25 13:48:41 +0200

    fixing a new bug after the last bug fix :D

--- BadWeatherApp/js/main.js
+++ BadWeatherApp/js/main.js
@@ -6,7 +6,9 @@ $(".dropdown-menu").on("click", "a", function () {
     clearWeatherContent();
     $('.tab-active').removeClass('tab-active').addClass('tab-inactive');
     $('#now-tab').addClass('tab-active').removeClass('tab-inactive');
-    
+    $('.active-content').removeClass('active-content').addClass('inactive-content');
+    $('#content-current').removeClass('inactive-content').addClass('active-content');
+
     onButtonClick(cityId);
 });
 
