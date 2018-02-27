406e9125053effd970b4c7259c764df0c727a9ef
Nikolay Nikolov <nikolaytsnikolov@gmail.com>
2018-02-27 11:55:20 +0200

    data changed to JSON format

--- BadWeatherApp/js/data.js
+++ BadWeatherApp/js/data.js
@@ -1,8 +1,425 @@
-var cities = [
-    { value: 'Vaduz', data : '3042030' },
-    { value: 'Vilnius', data : '593116' },
-    { value: 'Luxembourg', data : '2960316' },
-    { value: 'Skopje', data : '785842' },
-    { value: 'Valletta', data : '2562305' },
-    
+var cities = [{
+        value: 'Vaduz',
+        data: '3042030'
+    },
+    {
+        value: 'Vilnius',
+        data: '593116'
+    },
+    {
+        value: 'Luxembourg',
+        data: '2960316'
+    },
+    {
+        value: 'Skopje',
+        data: '785842'
+    },
+    {
+        value: 'Kuala Lumpur',
+        data: '1733046'
+    },
+    {
+        value: 'Valletta',
+        data: '2562305'
+    },
+    {
+        value: 'Mexico city',
+        data: '3530597'
+    },
+    {
+        value: 'Chisinau',
+        data: '618426'
+    },
+    {
+        value: 'Monaco',
+        data: '2993458'
+    },
+    {
+        value: 'Ulaanbaatar',
+        data: '2028462'
+    },
+    {
+        value: 'Podgorica',
+        data: '3193044'
+    },
+    {
+        value: 'Rabat',
+        data: '2538475'
+    },
+    {
+        value: 'Kathmandu',
+        data: '1283241'
+    },
+    {
+        value: 'Abuja',
+        data: '2352778'
+    },
+    {
+        value: 'Pyongyang',
+        data: '1871859'
+    },
+    {
+        value: 'Muscat',
+        data: '287286'
+    },
+    {
+        value: 'Oslo',
+        data: '6453366'
+    },
+    {
+        value: 'Islamabad',
+        data: '1176615'
+    },
+    {
+        value: 'Panama',
+        data: '4167694'
+    },
+    {
+        value: 'Manila',
+        data: '1701668'
+    },
+    {
+        value: 'Lisbon',
+        data: '2267057'
+    },
+    {
+        value: 'Warsaw',
+        data: '756135'
+    },
+    {
+        value: 'Doha',
+        data: '290030'
+    },
+    {
+        value: 'Bucharest',
+        data: '683506'
+    },
+    {
+        value: 'Moscow',
+        data: '524901'
+    },
+    {
+        value: 'Yakutsk',
+        data: '2013159'
+    },
+    {
+        value: 'San Marino',
+        data: '3168070'
+    },
+    {
+        value: 'Sao Tome',
+        data: '2410764'
+    },
+    {
+        value: 'Belgrade',
+        data: '792680'
+    },
+    {
+        value: 'Riyadh',
+        data: '108410'
+    },
+    {
+        value: 'Singapore',
+        data: '1880252'
+    },
+    {
+        value: 'Bratislava',
+        data: '3060972'
+    },
+    {
+        value: 'Ljubljana',
+        data: '3196359'
+    },
+    {
+        value: 'Mogadishu',
+        data: '53654'
+    },
+    {
+        value: 'Pretoria',
+        data: '964137'
+    },
+    {
+        value: 'Cape Town',
+        data: '3369157'
+    },
+    {
+        value: 'Seoul',
+        data: '1835848'
+    },
+    {
+        value: 'Madrid',
+        data: '3117735'
+    },
+    {
+        value: 'Barcelona',
+        data: '6356055'
+    },
+    {
+        value: 'Sevilla',
+        data: '2510911'
+    },
+    {
+        value: 'Stockholm',
+        data: '2673730'
+    },
+    {
+        value: 'Damascus',
+        data: '170654'
+    },
+    {
+        value: 'Taipei',
+        data: '1668341'
+    },
+    {
+        value: 'Tunis',
+        data: '2464470'
+    },
+    {
+        value: 'Ankara',
+        data: '323786'
+    },
+    {
+        value: 'Istanbul',
+        data: '745044'
+    },
+    {
+        value: 'Izmir',
+        data: '311046'
+    },
+    {
+        value: 'Kiev',
+        data: '703448'
+    },
+    {
+        value: 'Washington',
+        data: '5549222'
+    },
+    {
+        value: 'Seattle',
+        data: '5809844'
+    },
+    {
+        value: 'Las Vegas',
+        data: '5475433'
+    },
+    {
+        value: 'Miami ',
+        data: '5526233'
+    },
+    {
+        value: 'Los Angeles',
+        data: '5368361'
+    },
+    {
+        value: 'San Francisco',
+        data: '5391959'
+    },
+    {
+        value: 'Montevideo',
+        data: '3441575'
+    },
+    {
+        value: 'Tashkent',
+        data: '1512569'
+    },
+    {
+        value: 'Caracas',
+        data: '3646738'
+    },
+    {
+        value: 'Kabul',
+        data: '1138958'
+    },
+    {
+        value: 'Tirana',
+        data: '3183875'
+    },
+    {
+        value: 'Algiers',
+        data: '2507480'
+    },
+    {
+        value: 'Buenos Aires',
+        data: '3435910'
+    },
+    {
+        value: 'Yerevan',
+        data: '616051'
+    },
+    {
+        value: 'Canberra',
+        data: '2172517'
+    },
+    {
+        value: 'Melbourne',
+        data: '6952201'
+    },
+    {
+        value: 'Sydney',
+        data: '2147714'
+    },
+    {
+        value: 'Vienna',
+        data: '2761369'
+    },
+    {
+        value: 'Baku',
+        data: '587081'
+    },
+    {
+        value: 'Dhaka',
+        data: '1185241'
+    },
+    {
+        value: 'Minsk',
+        data: '625144'
+    },
+    {
+        value: 'Sarajevo',
+        data: '3191281'
+    },
+    {
+        value: 'Rio de Janeiro',
+        data: '3451190'
+    },
+    {
+        value: 'Brasilia',
+        data: '3469058'
+    },
+    {
+        value: 'Beijing',
+        data: '2038349'
+    },
+    {
+        value: 'San Jose',
+        data: '3837239'
+    },
+    {
+        value: 'Zagreb',
+        data: '3186886'
+    },
+    {
+        value: 'Havana',
+        data: '3553478'
+    },
+    {
+        value: 'Prague',
+        data: '3067696'
+    },
+    {
+        value: 'Copenhagen',
+        data: '2618425'
+    },
+    {
+        value: 'Djibouti',
+        data: '223817'
+    },
+    {
+        value: 'Santo Domingo',
+        data: '3668373'
+    },
+    {
+        value: 'Quito',
+        data: '3652462'
+    },
+    {
+        value: 'Cairo',
+        data: '360630'
+    },
+    {
+        value: 'Tallinn',
+        data: '588409'
+    },
+    {
+        value: 'Helsinki',
+        data: '658226'
+    },
+    {
+        value: 'Paris',
+        data: '2968815'
+    },
+    {
+        value: 'Tbilisi',
+        data: '611717'
+    },
+    {
+        value: 'Accra',
+        data: '2306104'
+    },
+    {
+        value: 'Athens',
+        data: '264371'
+    },
+    {
+        value: 'Budapest',
+        data: '3054643'
+    },
+    {
+        value: 'Reykjavik',
+        data: '3413829'
+    },
+    {
+        value: 'New Delhi',
+        data: '1261481'
+    },
+    {
+        value: 'Jakarta',
+        data: '1642911'
+    },
+    {
+        value: 'Tehran',
+        data: '112931'
+    },
+    {
+        value: 'Baghdad',
+        data: '98182'
+    },
+    {
+        value: 'Dublin',
+        data: '2964574'
+    },
+    {
+        value: 'Jerusalem',
+        data: '281184'
+    },
+    {
+        value: 'Tokyo',
+        data: '1850147'
+    },
+    {
+        value: 'Kingston',
+        data: '7839796'
+    },
+    {
+        value: 'Astana',
+        data: '1526273'
+    },
+    {
+        value: 'Nairobi',
+        data: '184745'
+    },
+    {
+        value: 'Pristina',
+        data: '786714'
+    },
+    {
+        value: 'Kuwait',
+        data: '285570'
+    },
+    {
+        value: 'Riga',
+        data: '456172'
+    },
+    {
+        value: 'Beirut',
+        data: '276781'
+    },
+    {
+        value: 'Tripoli',
+        data: '221024'
+    },
+    {
+        value: 'Borovets',
+        data: '733055'
+    }
 ];
