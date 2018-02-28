99049429f9727c61fe55a837cff3e1fe20145354
Nikolay Nikolov <nikolaytsnikolov@gmail.com>
2018-02-28 15:10:10 +0200

    imageController refactor

--- BadWeatherApp/js/imageController.js
+++ BadWeatherApp/js/imageController.js
@@ -4,8 +4,12 @@ let imageController = (function () {
 
         let hour = parseInt((json.list[0].dt_txt.split(' ')[1].substr(0, 5)).split(':')[0]);
         let weather = (json.list[0].weather[0].main);
-
-        if (hour > 6 && hour <= 8) {
+        let morningHours = hour > 6 && hour <= 8;
+        let dayHours = hour > 8 && hour <= 18;
+        let eveningHours = hour > 18 && hour <= 20;
+        let nightHours = hour <= 6 || hour > 20;
+        
+        if (morningHours) {
             if (weather === 'Snow') {
                 $('#body').css('background-image', 'url(images/giphy.gif');
             };
@@ -18,7 +22,7 @@ let imageController = (function () {
             if (weather === 'Clear') {
                 $('#body').css('background-image', 'url(images/clear/morning1.jpg');
             };
-        } else if (hour > 8 && hour <= 18) {
+        } else if (dayHours) {
             if (weather === 'Snow') {
                 $('#body').css('background-image', 'url(images/giphy.gif');
             };
@@ -31,7 +35,7 @@ let imageController = (function () {
             if (weather === 'Clear') {
                 $('#body').css('background-image', 'url(images/clear/sunny.jpg');
             };
-        } else if (hour > 18 && hour <= 20) {
+        } else if (eveningHours) {
             if (weather === 'Snow') {
                 $('#body').css('background-image', 'url(images/giphy.gif');
             };
@@ -44,7 +48,7 @@ let imageController = (function () {
             if (weather === 'Clear') {
                 $('#body').css('background-image', 'url(images/clear/evening1.jpg');
             };
-        } else if (hour <= 6 || hour > 20) {
+        } else if (nightHours) {
             if (weather === 'Snow') {
                 $('#body').css('background-image', 'url(images/giphy.gif');
             };
@@ -60,30 +64,25 @@ let imageController = (function () {
         };
     });
 
-    let setWeatherIcon = function (weather, i ) {
+    let setWeatherIcon = function (weather, i) {
 
         ////Changes Icons for tab "Now"
-        
+
         
         if (weather === 'Clouds') {
             $(`#icon-${i}`).attr('src', 'images/clouds.png');
-        }
-        else if (weather === 'Snow') {
-            $(`#icon-${i}`).attr('src', 'images/snow.png'); 
-        }
-        else if (weather === 'Rain') {
+        } else if (weather === 'Snow') {
+            $(`#icon-${i}`).attr('src', 'images/snow.png');
+        } else if (weather === 'Rain') {
             $(`#icon-${i}`).attr('src', 'images/rain.png');
-        } 
-        else if (weather === 'Clear') {
+        } else if (weather === 'Clear') {
             $(`#icon-${i}`).attr('src', 'images/clear.png');
-        } 
-        else if (weather === 'Storm') {
+        } else if (weather === 'Storm') {
             $(`#icon-${i}`).attr('src', 'images/storm.png');
-        }
-        else {
+        } else {
             $(`#icon-${i}`).attr('src', 'images/other.png');
         };
-       
+
 
         ////Changes Icons for tab "five-days"
 
@@ -117,6 +116,6 @@ let imageController = (function () {
     };
     return {
         SetBackground: createDeafultBackground,
-        SetWeatherIcon : setWeatherIcon
+        SetWeatherIcon: setWeatherIcon
     };
 }());
